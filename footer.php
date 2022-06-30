806221e7a35ed0ef8623d2b8aaf4edfcb78378fb
Shazib-Wani <shazib20cs202@newhorizonindia.edu>
2022-07-01 01:33:05 +0530

    footer fianl

--- footer.php
+++ footer.php
@@ -11,10 +11,10 @@
                         <span>No-26/1, 5th Floor, WTC-Brigade, Dr. Rajkumar Road, Malleshwaram, Bangalore-560055</span>
                     </div>
                     <div class="d-flex pt-1 m-n1">
-                        <a class="btn btn-lg-square text-primary m-1" href=""><i class="fab fa-twitter"></i></a>
-                        <a class="btn btn-lg-square text-primary m-1" href=""><i class="fab fa-facebook-f"></i></a>
-                        <a class="btn btn-lg-square text-primary m-1" href=""><i class="fab fa-youtube"></i></a>
-                        <a class="btn btn-lg-square text-primary m-1" href=""><i class="fab fa-linkedin-in"></i></a>
+                        <a class="btn btn-lg-square text-primary m-1" href="https://twitter.com/ieee_section"><i class="fab fa-twitter"></i></a>
+                        <a class="btn btn-lg-square text-primary m-1" href="https://www.facebook.com/IEEEBangaloreSection/"><i class="fab fa-facebook-f"></i></a>
+                        <a class="btn btn-lg-square text-primary m-1" href="https://www.youtube.com/channel/UCP5O7GrQaAgAPjvNIRhuiyQ"><i class="fab fa-youtube"></i></a>
+                        <a class="btn btn-lg-square text-primary m-1" href="https://www.linkedin.com/company/ieee-bangalore-section/"><i class="fab fa-linkedin-in"></i></a>
                     </div>
                     <div class="d-flex align-items-center mb-2">
                        
@@ -24,19 +24,19 @@
                 </div>
                 <div class="col-12 col-md-3">
                     <h4 class="text-uppercase mb-4 text-dark">CONNECT WITH IEEE</h4>
-                    <a class="btn btn-link text-primary" href="">IEEE Collabratec</a>
-                    <a class="btn btn-link text-primary" href="">Careers at IEEE us</a>
-                    <a class="btn btn-link text-primary" href="">IEEE Newsroom</a>
-                    <a class="btn btn-link text-primary" href="">IEEE Media Kit</a>
-                    <a class="btn btn-link text-primary" href="">IEEE Learning Network</a>
+                    <a class="btn btn-link text-primary" href="https://ieee-collabratec.ieee.org/?utm_source=dhtml_footer&utm_medium=hp&utm_campaign=collabratec">IEEE Collabratec</a>
+                    <a class="btn btn-link text-primary" href="https://ieee.taleo.net/careersection/2/jobsearch.ftl?utm_source=mf&utm_campaign=taleo-jobs&utm_medium=footer&utm_term=taleo-jobs%20at%20ieee">Careers at IEEE us</a>
+                    <a class="btn btn-link text-primary" href="https://www.ieee.org/about/news/index.html?utm_source=dhtml_footer&utm_medium=hp&utm_campaign=newsroom">IEEE Newsroom</a>
+                    <a class="btn btn-link text-primary" href="https://www.ieee.org/about/news/media-kit/media-kit-index.html?utm_source=dhtml_footer&utm_medium=hp&utm_campaign=media-kit">IEEE Media Kit</a>
+                    <a class="btn btn-link text-primary" href="https://iln.ieee.org/public/TrainingCatalog.aspx">IEEE Learning Network</a>
                 </div>
                 <div class="col-12 col-md-3">
                     <h4 class="text-uppercase mb-4 text-dark">MEMBERSHIP</h4>
-                    <a class="btn btn-link text-primary" href="">Join</a>
-                    <a class="btn btn-link text-primary" href="">Renew</a>
-                    <a class="btn btn-link text-primary" href="">Benefits</a>
-                    <a class="btn btn-link text-primary" href="">IEEE Collabratec</a>
-                    <a class="btn btn-link text-primary" href="">Guide to Join IEEE</a>
+                    <a class="btn btn-link text-primary" href="https://www.ieee.org/membership/join/index.html?utm_source=dhtml_footer&utm_medium=hp&utm_campaign=join">Join</a>
+                    <a class="btn btn-link text-primary" href="https://www.ieee.org/membership/renew.html?utm_source=dhtml_footer&utm_medium=hp&utm_campaign=renew">Renew</a>
+                    <a class="btn btn-link text-primary" href="https://www.ieee.org/membership/benefits/index.html">Benefits</a>
+                    <a class="btn btn-link text-primary" href="https://ieee-collabratec.ieee.org/?utm_source=dhtml_footer&utm_medium=hp&utm_campaign=collabratec-membership-list">IEEE Collabratec</a>
+                    <a class="btn btn-link text-primary" href="https://ieeebangalore.org/guide-to-join-ieee/">Guide to Join IEEE</a>
                 </div>
                 <div class="col-12 col-md-3">
                     <h4 class="text-uppercase mb-4 text-dark">Quick Links</h4>
