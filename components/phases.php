b1dd4bf6e2b37fc1c2cc9a5cf5fd50d4c0d351ac
anzar00 <peerzada.anzar@gmail.com>
2022-08-25 05:21:18 +0530

    phases

---
+++ components/phases.php
@@ -0,0 +1,77 @@
+<div id="participation" class="container-xxl py-5">
+    <div class="container">
+        <div class="text-center mx-auto mb-5 wow fadeInUp" data-wow-delay="0.1s" style="max-width: 600px;">
+            <p class="d-inline-block bg-secondary text-primary py-1 px-4">Participation</p>
+            <h1 class="text-uppercase">Participation and Phases</h1>
+        </div>
+        <div class="row g-4">
+            <div class="d-flex flex-column">
+                <div class="w-100 bg-secondary p-5">
+                    <h1 style="color:black" class="mb-2">Participation</h1>
+                    <ul style="font-size: 20px;">
+                        <li class="text-primary mb-3">Teams to comprise of 4 members; with at least one-woman participant.</li>
+                        <li class="text-primary mb-3">The event is open to all Students of Engineering (BTech and MTech).</li>
+                        <li class="text-primary mb-3">Students from all years and all branches are eligible to participate in the contests.</li>
+                        <li class="text-primary mb-3">A seriousness fees will be charged to ensure participation and student engagement.</li>
+                    </ul>
+                    <br>
+                    Attractive prizes will be given to the winning teams along with participation certificates for all the participants. The event is designed to provide AICTE activity points to students to ensure good participation.
+                    <h1 style="color: black;" class="mb-2 mt-2">Phases</h1>
+                    <!-- Table for phases -->
+                    <table class="table table-bordered table-striped mt-2">
+                        <thead>
+                            <tr>
+                                <th scope="col">Contest</th>
+                                <th scope="col">Description</th>
+                                <th scope="col">Timelines</th>
+                                <th scope="col">Expected Outcome</th>
+                                <th scope="col">Deliverables on Completion</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <th scope="row">Contest 1</th>
+                                <td>Problem Scoping - Identify the problem in the given focus areas.</td>
+                                <td>15th Aug - 31st Aug, 2022</td>
+                                <td>
+                                    <ul>
+                                        <li>Poster</li>
+                                        <li>Problem Presentation</li>
+                                    </ul>
+                                </td>
+                                <td>Participation certificate AICTE Activity Points: 5</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">Contest 2</th>
+                                <td>Ideation Identify the problem from CMT database</td>
+                                <td>15th Aug - 31st Aug, 2022</td>
+                                <td>
+                                    <ul>
+                                        <li>Abstract</li>
+                                        <li>Tools that could be used</li>
+                                        <li>Breakup of the timeline in terms of tasks to be achieved every 2 weeks</li>
+                                        <li>Budget</li>
+                                    </ul>
+                                </td>
+                                <td>Shortlisted projects will receive Prizes, Certificate and AICTE Activity Points: 5</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">Contest 3</th>
+                                <td>Implementation</td>
+                                <td>1st Sept - 30th Oct, 2022</td>
+                                <td>
+                                    <ul>
+                                        <li>Showcase the completed project</li>
+                                    </ul>
+                                </td>
+                                <td>AICTE activity points</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+
+
+    </div>
+</div>
--- index.php
+++ index.php
@@ -34,95 +34,13 @@
   <?php include './components/themes.php' ?>
   <!-- Themes End -->
 
-  <!-- About Start -->
-  <!-- About End -->
-
-
-
-
-
-
-
-
-
-
-  <!-- Working Hours Start -->
-  <!-- Working Hours Start -->
-  <!-- <div class="container-xxl py-5">
-    <div class="container">
-      <div class="row g-0">
-        <div class="col-lg-6 wow fadeIn" data-wow-delay="0.1s">
-          <div class="h-100">
-            <img class="img-fluid h-100" src="img/971.jpg" alt="">
-          </div>
-        </div>
-        <div class="col-lg-6 wow fadeIn" data-wow-delay="0.5s">
-          <div class="bg-secondary h-100 d-flex flex-column justify-content-center p-5">
-            <p class="d-inline-flex bg-dark text-primary py-1 px-4 me-auto">Welcoming Ideas On:</p>
-            <div>
-              <div class="d-flex justify-content-between border-bottom py-2">
-
-                <span class="text-uppercase">Smart Farming</span>
-              </div>
-              <div class="d-flex justify-content-between border-bottom py-2">
-
-                <span class="text-uppercase">Education</span>
-              </div>
-              <div class="d-flex justify-content-between border-bottom py-2">
-
-                <span class="text-uppercase">Sustainable Development - Urban & Rural</span>
-              </div>
-              <div class="d-flex justify-content-between border-bottom py-2">
-
-                <span class="text-uppercase">Smart Healthcare</span>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div> -->
-  <!-- Working Hours End -->
-
-  <!-- Working Hours End -->
-
-
-  <!-- Testimonial Start -->
-  <!-- <section id="criteria" class="criteria section-bg">
-    <div class="container-xxl py-5">
-      <div class="container">
-        <div class="text-center mx-auto mb-5 wow fadeInUp" data-wow-delay="0.1s" style="max-width: 600px;">
-          <p class="d-inline-block bg-secondary text-primary py-1 px-4">For the Hackathon</p>
-          <h1 class="text-uppercase">Things to Know</h1>
-        </div>
-
-        <div class="tab-content" id="myTabContent">
-          <div class="tab-pane fade show active mt-5 section-title" id="home-tab-pane" role="tabpanel" aria-labelledby="home-tab" tabindex="0">
-            <p>
-            <h2>PARTICIPATION</h2>
-
-            <ul>
-              <li>Teams to comprise of 4 members; with at least one-woman participant.</li>
-              <li>The event is open to all Students of Engineering (BTech and MTech).</li>
-              <li>Students from all years and all branches are eligible to participate in the contests.</li>
-              <li>A seriousness fees will be charged to ensure participation and student engagement.</li>
-            </ul>
-            <br>
-            Attractive prizes will be given to the winning teams along with participation certificates for all the participants. The event is designed to provide AICTE activity points to students to ensure good participation.
-
-
-            </p>
-          </div>
-        </div>
-      </div>
-    </div>
-  </section> -->
-  <!-- Testimonial End -->
-
+  <!-- Participation and Phases Start -->
+  <?php include './components/phases.php' ?>
+  <!-- Participation and Phases End -->
 
   <!-- Contact Us Start -->
- <?php include './components/contact.php' ?>
-
+  <?php include './components/contact.php' ?>
+  <!-- Contact Us End -->
 
   <?php include 'footer.php' ?>
 </body>
