39f3c187e6da7968ce9146fefcd42e3c690d8935
anzar00 <peerzada.anzar@gmail.com>
2022-08-25 04:55:01 +0530

    contact

---
+++ components/contact.php
@@ -0,0 +1,15 @@
+<section id="contact">
+     <div class="container-xxl py-5">
+         <div class="container">
+             <div class="text-center mx-auto mb-5 wow fadeInUp" data-wow-delay="0.1s" style="max-width: 600px;">
+                 <p class="d-inline-block bg-secondary text-primary py-1 px-4">Contact</p>
+                 <h1 class="text-uppercase">For Any Queries</h1>
+             </div>
+             <div class="testimonial-item text-center" data-dot="<img class='img-fluid' src='img/testimonial-1.jpg' alt=''>">
+                 <h4 class="text-uppercase">Email us at</h4>
+                 <p class="text-primary"><a href="mailto:kumudh.r@ieee.org">kumudh.r@ieee.org</a></p>
+                 <p class="text-primary"><a href="mailto:prachako@in.ibm.com">prachako@in.ibm.com</a></p>
+             </div>
+         </div>
+     </div>
+ </section>
